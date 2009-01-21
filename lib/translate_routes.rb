122a44eadef8dbf701df5bc32a99466007ebfa9b
Christian Hølmer <Christian@hoelmer.dk>
2009-01-21 16:14:25 +0100

    Added optional scope for translation backend.

--- lib/translate_routes.rb
+++ lib/translate_routes.rb
@@ -13,6 +13,10 @@ module ActionController
       mattr_accessor :locale_param_key
       @@locale_param_key = :locale  # set to :locale for params[:locale]
 
+      # Optional scope for translation backend
+      mattr_accessor :translation_scope
+      @@translation_scope = ''
+
       mattr_accessor :original_routes
 
       mattr_accessor :dictionaries
@@ -143,7 +147,7 @@ module ActionController
           if @using_i18n
             tmp = I18n.locale
             I18n.locale = locale
-            value = I18n.t segment.value, :default => segment.value.dup
+            value = I18n.t segment.value, :default => segment.value.dup, :scope => @@translation_scope
             I18n.locale = tmp
           else
             value = @@dictionaries[locale][segment.value] || segment.value.dup
